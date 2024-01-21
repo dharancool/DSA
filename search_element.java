6a00d39985a2f108fd0063578f017bcfadf586c7
dharancool <109385336+dharancool@users.noreply.github.com>
2024-01-21 12:35:12 +0530

    Create search_element.java

---
+++ search_element.java
@@ -0,0 +1,10 @@
+ // Your code here
+        //int index=0;
+        for(int i=0;i<N;i++){
+            if(arr[i]==X){
+                //index=i;
+                return i;
+            }
+            
+        }
+        return -1;
